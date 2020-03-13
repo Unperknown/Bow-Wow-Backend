6b334e491493de1d847b4f1756694fefecd62ef3
Unperknown <wnsdud105@gmail.com>
2020-03-13 23:54:46 +0900

    [2] Added : Koa

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello, world!");
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import * as Koa from 'koa';
+
+const PORT = 8080;
+
+const app = new Koa();
+
+app.listen(PORT, () => {
+    console.log(`Server started at port ${PORT}`);
+});
