aaa251c09dfdcf961577abe4f3a3ed914b056daa
Unperknown <wnsdud105@gmail.com>
2020-03-14 23:50:19 +0900

    [3] Added : GraphQL

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,38 @@ import * as dotenv from 'dotenv';
 dotenv.config();
 
 import * as Koa from 'koa';
+import * as Router from 'koa-router';
+import * as koaBody from 'koa-bodyparser';
+import * as cors from '@koa/cors';
 
-const PORT = 8080;
+import * as mongoose from 'mongoose';
+
+import { ApolloServer } from 'apollo-server-koa';
+
+mongoose.connect(process.env.MONGODB_URI || '', {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(response => {
+  console.log('Successfully connected to MongoDB');
+}).catch(e => {
+  console.error(e);
+});
 
 const app = new Koa();
+const server = new ApolloServer({
 
-app.listen(PORT, () => {
-    console.log(`Server started at port ${PORT}`);
 });
+
+server.applyMiddleware({ app });
+
+app.use(cors())
+  .use(koaBody());
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Server started at port ${process.env.PORT}`);
+});
+
+app.on('error', err => {
+  console.error(err);
+})
