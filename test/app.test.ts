2b39bb7f18b9937f1ad5f2c92bb9a5e571000d82
Unperknown <wnsdud105@gmail.com>
2020-03-23 20:52:27 +0900

    [8] Added : Mocha Test

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,26 +1,39 @@
-import * as request from 'supertest';
+import 'mocha';
+
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+
 import app from '../src/app';
+
+const expect = chai.expect;
+chai.use(chaiHttp);
 
 describe('Bow-Wow Server', () => {
   describe('Server works', () => {
     it('When it started', async () => {
-      const response = await request(app.callback()).get('/graphql');
-
-      expect(response.status).toBe(200);
+      chai.request(app.callback())
+        .get('/graphql')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+        });
     });
   });
   describe('MongoDB works', () => {
     it('When it started', async () => {
-      const response = await request(app.callback()).get('/graphql');
-
-      expect(response.status).toBe(200);
+      chai.request(app.callback())
+        .get('/graphql')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+        });
     });
   });
   describe('GraphQL works', () => {
     it('When it started', async () => {
-      const response = await request(app.callback()).get('/graphql');
-
-      expect(response.status).toBe(200);
+      chai.request(app.callback())
+        .get('/graphql')
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+        });
     });
   });
 });
