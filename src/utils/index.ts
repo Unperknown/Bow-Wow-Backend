f301edc9ba884f46f1f6ca8cee3b5414366eb00f
Unperknown <wnsdud105@gmail.com>
2020-04-05 21:38:57 +0900

    [20] Added : Auth Utils

---
+++ src/utils/index.ts
@@ -0,0 +1,14 @@
+import * as jwt from 'jsonwebtoken';
+import { Context, Next } from 'koa';
+
+const APP_SECRET = process.env.APP_SECRET || 'Bow-Wow';
+
+export const getUserID = async (ctx: Context, next: Next) => {
+  const token = ctx.request.headers.authorization;
+
+  ctx.assert(token, 401, 'Error');
+
+  const userID = jwt.verify(token, APP_SECRET);
+
+  return userID;
+}
