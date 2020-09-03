db9d03f35725bcb1353bece53fe363df8e0e817e
unperknown <unperknown@icloud.com>
2020-09-03 12:01:34 +0900

    update: validate schema when testing

--- handler.ts
+++ handler.ts
@@ -2,22 +2,20 @@ import { dynamoDB, inputs } from './lib';
 import { response } from './middlewares';
 
 // TO-DO: APIGatewayEvent interface 재정의
-const graphql = response(
-  async (event: any) => {
-    const functions: String = event.function;
-    const data = JSON.parse(event.body);
+const graphql = response(async (event: any) => {
+  const functions: String = event.function;
+  const data = JSON.parse(event.body);
 
-    let input;
-
-    // TO-DO: Controller로 분리
-    if (functions == 'createUser') {
-      input = inputs.createUserItemInput(data);
-    }
+  // TO-DO: Controller로 분리
+  if (functions == 'createUser') {
+    const input = inputs.createUserItemInput(data);
 
     await dynamoDB.put(input);
 
     return input.Item;
   }
-);
+});
 
-export { graphql };
+export {
+  graphql,
+};
