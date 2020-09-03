9748c5aac6259ed786fa8e592a9452727b0da3e9
unperknown <unperknown@icloud.com>
2020-09-04 08:14:45 +0900

    fix: call dynamodb lib promise

--- lib/dynamodb.ts
+++ lib/dynamodb.ts
@@ -1,13 +1,14 @@
 import { DynamoDB } from 'aws-sdk';
+import { BatchGetItemInput, BatchWriteItemInput, DeleteItemInput, GetItemInput, PutItemInput, QueryInput, UpdateItemInput } from 'aws-sdk/clients/dynamodb';
 
 const dynamoDB = new DynamoDB.DocumentClient();
 
 export default {
-  get: (input: any) => dynamoDB.get(input).promise,
-  batchGet: (input: any) => dynamoDB.batchGet(input).promise,
-  put: (input: any) => dynamoDB.put(input).promise,
-  batchWrite: (input: any) => dynamoDB.batchWrite(input).promise,
-  query: (input: any) => dynamoDB.query(input).promise,
-  update: (input: any) => dynamoDB.update(input).promise,
-  delete: (input: any) => dynamoDB.delete(input).promise,
+  get: (input: GetItemInput) => dynamoDB.get(input).promise(),
+  batchGet: (input: BatchGetItemInput) => dynamoDB.batchGet(input).promise(),
+  put: (input: PutItemInput) => dynamoDB.put(input).promise(),
+  batchWrite: (input: BatchWriteItemInput) => dynamoDB.batchWrite(input).promise(),
+  query: (input: QueryInput) => dynamoDB.query(input).promise(),
+  update: (input: UpdateItemInput) => dynamoDB.update(input).promise(),
+  delete: (input: DeleteItemInput) => dynamoDB.delete(input).promise(),
 };
