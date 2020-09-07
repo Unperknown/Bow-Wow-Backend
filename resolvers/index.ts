8e30cb4f1a20b392951837de619f31d749245593
unperknown <unperknown@icloud.com>
2020-09-07 14:20:20 +0900

    add: resolvers

--- resolvers/index.ts
+++ resolvers/index.ts
@@ -1,8 +1,17 @@
-import { createUser, createPet, closeUser, deletePet } from './mutations';
+import {
+  createUser,
+  createPet,
+  closeUser,
+  deletePet,
+  createPost,
+  createAlarm,
+  deleteAlarm,
+  deletePost,
+} from './mutations';
 
 const mappedResolverWithRequest = async (payload: any) => {
   let result;
-  
+
   switch (payload.field) {
   case 'createUser':
     result = await createUser(payload);
@@ -10,17 +19,27 @@ const mappedResolverWithRequest = async (payload: any) => {
   case 'createPet':
     result = await createPet(payload);
     break;
+  case 'createPost':
+    result = await createPost(payload);
+    break;
+  case 'createAlarm':
+    result = await createAlarm(payload);
+    break;
   case 'closeUser':
     result = await closeUser(payload);
     break;
   case 'deletePet':
     result = await deletePet(payload);
     break;
+  case 'deletePost':
+    result = await deletePost(payload);
+    break;
+  case 'deleteAlarm':
+    result = await deleteAlarm(payload);
+    break;
   }
 
   return result;
 };
 
-export { 
-  mappedResolverWithRequest,
-};
+export { mappedResolverWithRequest };
--- resolvers/mutations.ts
+++ resolvers/mutations.ts
@@ -16,6 +16,22 @@ const createPet = async (payload: any) => {
   return result.$response.data;
 };
 
+const createPost = async (payload: any) => {
+  const input = inputs.createPostItemInput(payload);
+  
+  await dynamoDB.put(input);
+
+  return input.Item;
+};
+
+const createAlarm = async (payload: any) => {
+  const input = inputs.createAlarmItemInput(payload);
+
+  const result = await dynamoDB.update(input);
+
+  return result.$response.data;
+};
+
 const closeUser = async (payload: any) => {
   const input = inputs.closeUserItemInput(payload);
 
@@ -32,9 +48,29 @@ const deletePet = async (payload: any) => {
   return result.$response.data;
 };
 
+const deletePost = async (payload: any) => {
+  const input = inputs.deletePostItemInput(payload);
+
+  const result = await dynamoDB.delete(input);
+
+  return result.$response.data;
+};
+
+const deleteAlarm = async (payload: any) => {
+  const input = inputs.deleteAlarmItemInput(payload);
+
+  const result = await dynamoDB.update(input);
+
+  return result.$response.data;
+};
+
 export {
   createUser,
   createPet,
+  createPost,
+  createAlarm,
   closeUser,
   deletePet,
+  deletePost,
+  deleteAlarm,
 };
